6154fe6a7977f604932906df497c552b34999fd3
Alex Mihaescu <alexcugir_ps4@yahoo.com>
2019-11-17 14:53:23 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# Simple Python Weather App
+
+## Prerequisites
+
+- Python 3.7
+- Flask
+- requests
+
+## Env Vars
+
+To run in debug mode:
+
+`$ set FLASK_ENV=development`
+
+To run Flash development server:
+
+`$set FLASK_yPP=app.py`
+
+## Local Development
+
+To start flask app:
+
+`$ flask run`
+
---
+++ app.py
@@ -0,0 +1,30 @@
+import json
+
+from flask import Flask
+
+import weather_app
+
+# print(" >>> ", sys.path)
+
+app = Flask(__name__)
+
+# Prima ruta care o ia din radacina din cauza la /
+
+@app.route("/")
+def hello():
+    return "Hello, World!"
+
+# A 2-a ruta care este verificate pe site doar cu /weather/ dupa ip
+
+@app.route("/weather/")
+def weather_route():
+    temp = json.dumps(weather_app.weather())
+    return temp
+
+# A 3-a ruta
+
+@app.route("/weather/my-cities/")
+def weather_multiple_citites():
+    return"Cluj:15, New York: 10"
+
+
